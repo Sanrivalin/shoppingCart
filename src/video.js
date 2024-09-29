751e97c821392df3b6cd8d05436ace777c57385c
Sanrivalin <santiagorivasmolina@gmail.com>
2024-09-28 23:48:33 -0500

    video id

---
+++ src/video.js
@@ -0,0 +1,15 @@
+    document.addEventListener('DOMContentLoaded', (event) => {
+    const myVideo = document.getElementById('myVideo');
+ 
+    myVideo.addEventListener('ended', (event) => {
+        console.log("Video has ended.");
+        // Execute specific code here
+        executeAfterVideo();
+    });
+});
+function executeAfterVideo() {
+    dataLayer.push({
+        'event': 'tutorial_complete',
+    });
+    console.log('Video has ended!');
+}
